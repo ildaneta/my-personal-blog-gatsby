95e87d97a697a18eeba6f382141bd9f492f6ccbb
Ilda Neta <iildaemanoely@gmail.com>
2020-06-04 18:48:53 -0300

    feat: creating a new description for the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,54 +24,51 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <AboutHeader>Sobre Mim</AboutHeader>
+    <AboutDescription>
+      Olá! Meu nome é {' '}
+      <AboutLink href="https://www.linkedin.com/in/ilda-silva-neta/" target="_blank">
+        Ilda Neta
+      </AboutLink>{' '},
+      atualmente vivo em Salvador-Bahia/Brasil e atuo como desenvolvedora Mobile com foco em React Native no{' '}
+      <AboutLink href="https://www.abcbrasil.com.br/" target="_blank">
+        Banco ABC (Arab Banking Corporation) Brasil.
+      </AboutLink>{' '}
+    </AboutDescription>
+    <AboutDescription>
+      Atuando há mais de 6 anos com tecnologia, aprendi que poderia impactar a vida das pessoas combinando tecnologia, educação financeira e investimentos, fazendo-me sentir que nasci para o que faço.
+    </AboutDescription>
 
     <AboutDescription>
-      Olá! Meu nome é Ilda Emanoely Neta, sou natural de Goiânia-Goiás/Brasil,
-      atualmente moro em Salvador-Bahia/Brasil e trabalho na{' '}
-      <AboutLink href="http://cubos.io/" target="_blank">
-        Cubos Tecnologia.
-      </AboutLink>{' '}
-      Descobri que através do estilo de vida minimalista e da educação
-      financeira, posso impactar a vida de muitas pessoas
+      Nasci para encantar e mudar a vida das pessoas, gerando impactos tecnológicos que podem modificar todo o ambiente em que vivem.
+
     </AboutDescription>
+
+
     <AboutDescription>
-      Atuo na área de tecnologia há 6 anos, onde já possuí os cargos de:
-      analista de processos, suporte e atualmente sou desenvolvedora front-end.
-      Atuando como analista de processos fui responsável por levantamento de
-      requisitos, criação e execução de testes e implantação.
+      Com isso, juntamente com minha equipe, estamos focados em causar uma revolução tecnológica, no produto de Investimentos de Renda Fixa do Banco ABC, chamada {' '}
+      <AboutLink href="https://www.abcbrasil.com.br/abc-personal/" target="_blank">
+        Personal
+      </AboutLink>{' '}.
     </AboutDescription>
+
     <AboutDescription>
-      Atuando como desenvolvedora front-end sou responsável pela criação de um
-      módulo de investimentos do nosso produto em parceria com o Banco Máxima,
-      que se chama BBNK. O BBNK é um white label bancário, onde alguns itens do
-      banco podem ser customizáveis a partir do desejo do cliente. O módulo de
-      investimentos que estou criando juntamente com mais um front-end, é
-      voltado para a Renda Fixa, para que o dinheiro do cliente fique rendendo
-      na conta até que ele liquide todo o valor.
+      Além de amar palestrar, descobri que poderia auxiliar ainda mais a comunidade transformando a paixão em escrever artigos em realidade através de um blog, surgindo assim meus primeiros códigos utilizando o Gatsby para tornar essa vontade uma
+      realidade.
     </AboutDescription>
+
     <AboutDescription>
-      Cursei Sistemas de Informação na{' '}
-      <AboutLink href="https://www.ufg.br" target="_blank">
-        Universidade Federal de Goiás
-      </AboutLink>{' '}
-      e atualmente a fim de aprofundar meus estudos na carreira de tecnologia,
-      estou cursando Gestão da Tecnologia da Informação no{' '}
-      <AboutLink href="http://unijorge.edu.br/" target="_blank">
-        Centro Universitário Jorge Amado.
-      </AboutLink>
-    </AboutDescription>
-    <AboutDescription>
-      Através da paixão em escrever artigos, surgiu em mim a vontade de criar
-      meu próprio Blog, onde utilizei o Gatsby para tornar essa vontade uma
-      realidade. Palestrar também me torna mais feliz, pois assim, posso
-      compartilhar experiências laborais e pessoais com nossa grandiosa
-      comunidade de tecnologia.{' '}
+
       <AboutLink
-        href="https://ildaneta.netlify.com/posts/minha-primeira-palestra-em-um-festival-de-tecnologia-para-mulheres/"
+        href="https://www.linkedin.com/posts/ilda-silva-neta_rocketseat-spacesquad-desenvolvimentopessoal-activity-6663898502927646720-boHI"
         target="_blank"
       >
-        Veja aqui um relato sobre minha primeira palestra.
+        Recentemente também fui convidada para participar do Space Squad da Rocketseat, como Mentora
       </AboutLink>
+      , através do voluntariado, para auxiliar outras(os) devs a conquistarem o próximo nível que tanto almejam
+    </AboutDescription>
+
+    <AboutDescription>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/U0wQjppkFdY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </AboutDescription>
     <AboutDescription>
       Espero te ver aqui mais vezes, pois esse blog foi feito com muito carinho,
