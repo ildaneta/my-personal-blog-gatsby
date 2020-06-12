1cc03f593eb92e7eb9abea1ebd37e8004b7b4172
Ilda Neta <iildaemanoely@gmail.com>
2020-06-12 11:53:40 -0300

    feat: add the analytics google plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     position: `Desenvolvedora Mobile`,
     description: `Uma desenvolvedora minimalista, que busca através da escrita demonstrar suas paixões por tecnologia, soft skills e educação financeira.`,
     author: `Ilda Neta`,
-    siteURL: `https://ildaneta.netlify.com/`
+    siteURL: `https://ildaneta.netlify.com/`,
   },
   plugins: [
     `gatsby-plugin-transition-link`,
@@ -19,22 +19,51 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
-        path: `${__dirname}/static/assets/img`
-      }
+        path: `${__dirname}/static/assets/img`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-120929041-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Enables Google Optimize Experiment ID
+        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'example.com',
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/posts`
-      }
+        path: `${__dirname}/posts`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -43,21 +72,21 @@ module.exports = {
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
-              name: 'uploads'
-            }
+              name: 'uploads',
+            },
           },
           {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 960,
-              linkImagesToOriginal: false
-            }
+              linkImagesToOriginal: false,
+            },
           },
 
           `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`
-        ]
-      }
+          `gatsby-remark-prismjs`,
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -69,8 +98,8 @@ module.exports = {
         indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
         queries,
         chunkSize: 10000, // quantidade de dados por vez
-        enablePartialUpdates: true
-      }
+        enablePartialUpdates: true,
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -82,12 +111,12 @@ module.exports = {
         theme_color: `#191921;`,
         display: `minimal-ui`,
         // favicon
-        icon: `src/images/favicon.png` // This path is relative to the root of the site.
-      }
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify-cms`
-  ]
+    `gatsby-plugin-netlify-cms`,
+  ],
 };
