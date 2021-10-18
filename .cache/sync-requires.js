258358588c06345ed9594941059dbf4c3920a894
ildaneta <iildaemanoely@gmail.com>
2021-10-18 17:55:32 -0300

    gatsby upgrade

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/ildaneta/Documents/Github/my-personal-blog-gatsby/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/ildaneta/Documents/Github/my-personal-blog-gatsby/.cache/dev-404-page.js")),
   "component---src-components-templates-blog-list-js": preferDefault(require("/Users/ildaneta/Documents/Github/my-personal-blog-gatsby/src/components/templates/blog-list.js")),
   "component---src-components-templates-blog-post-js": preferDefault(require("/Users/ildaneta/Documents/Github/my-personal-blog-gatsby/src/components/templates/blog-post.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/ildaneta/Documents/Github/my-personal-blog-gatsby/src/pages/404.js")),
