f3b88a207d4f01f3212388208e4db33908f5d3d2
ildaneta <iildaemanoely@gmail.com>
2022-10-07 23:22:36 +0200

    feat: create utils for talks and presentations content

---
+++ src/utils/talks.js
@@ -0,0 +1,113 @@
+export const talks = [
+  {
+    id: "1",
+    title: "JavaScript Netherlands 🇳🇱 2022 - JSWorld Online",
+    description: "How to study and reach your first job into IT area.",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194427878-2940d409-9693-4469-86f6-f277b6c9ca2a.png",
+    url: "https://www.linkedin.com/posts/ildaneta_javascriptworld-jsamsterdam-ildaneta-activity-6887010507035934720-yCqX?utm_source=share&utm_medium=member_desktop",
+  },
+  {
+    id: "2",
+    title: "The Developers Conference (TDC FUTURE 2021)",
+    description: "Como se tornar um(a) desenvolvedor(a) mobile.",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194152307-11b1a103-0675-4de2-8d09-e456369db36a.png",
+    url: "https://www.linkedin.com/posts/ildaneta_thedevelopersconferencebrasil-grupoboticaerrio-activity-6869258226894958593-5M__?utm_source=share&utm_medium=member_desktop",
+  },
+  {
+    id: "3",
+    title: "React Case Study Festival EUA 🇺🇸 2021",
+    description:
+      "Performing in a robust and scalable project by creating a components library.",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194151413-d4a65e13-10c5-4606-bc3e-6b56931dcef3.png",
+    url: "https://www.youtube.com/embed/hv0R-DddYv8?start=11987",
+  },
+  {
+    id: "4",
+    title: "React Conf Brasil 🇧🇷 2020",
+    description:
+      "Utilizando o REM para construir layouts responsivos com o React Native.",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194149415-e02a9ea6-00eb-46c2-8f70-f8435e8bd1aa.png",
+    url: "https://www.youtube.com/watch?v=K1TpmE-XNuA&list=PLVNlqEw0GNauq3lrezxdDmOz5_QJJy6IB&index=5",
+    date: "25/11/2020",
+  },
+  {
+    id: "5",
+    title: "FEMTEC BA (Festival Baiano de Mulheres na Tecnologia)",
+    description:
+      "Como lutar contra o complexo de inferioridade e conseguir atingir nossos objetivos.",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194646601-eff3a2fe-55c8-46eb-b090-bfcb1ffc8df8.png",
+    url: "https://ildaneta.dev/posts/minha-primeira-palestra-em-um-festival-de-tecnologia-para-mulheres",
+    date: "26/10/2019",
+  },
+];
+
+export const participations = [
+  {
+    id: "1",
+    title: "Design System - Meiuca",
+    description: "Aula aberta: O lado do código em um Design System",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194645215-5f59f245-795f-4139-be1e-ed12c6b9f418.png",
+    url: "https://www.youtube.com/watch?v=V6HZ6ptvTBc",
+    date: "16/02/2022",
+  },
+  {
+    id: "2",
+    title: "Conexão Dev E18 - Catapulta Club",
+    description: "Como iniciar desenvolvimento mobile com React Native",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194630879-4d5d5262-d35f-4d3a-af4e-15d9d03ceee2.png",
+    url: "https://www.youtube.com/watch?v=xvb15wFcOss",
+    date: "25/11/2021",
+  },
+  {
+    id: "3",
+    title: "FalaDev - Rocketseat",
+    description: "React Native - Boas práticas e construção de interfaces",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194655077-0d1e90b2-f01d-4e00-8d6b-9e03d9e336b7.png",
+    url: "https://www.youtube.com/watch?v=brBlwk_pRDI&list=PL85ITvJ7FLoivA_1YoBrRJbNWajIoR7Os&index=22",
+    date: "18/06/2021",
+  },
+  {
+    id: "4",
+    title: "Comemoração 4 anos - Rocketseat",
+    description: "4 anos rumo ao infinito 🚀",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194647898-305a3c4f-615a-457a-869d-26538c3810d9.png",
+    url: "https://www.youtube.com/watch?v=G-jBZWeSX-Q",
+    date: "28/05/2021",
+  },
+  {
+    id: "5",
+    title: "Alumni #02 - Rocketseat",
+    description: "Ilda Neta - Mobile Developer Banco ABC Brasil",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194653676-987c2fd7-8044-4449-9643-bc0c7a537aa6.png",
+    url: "https://www.youtube.com/watch?v=U0wQjppkFdY",
+    date: "29/05/2020",
+  },
+  {
+    id: "6",
+    title: "LIVE com Marcos Henrique e Ilda Neta",
+    description: "Dicas para se destacar profissionalmente",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194645487-835afe42-6687-4fef-a449-8e992ad3cb97.png",
+    url: "https://www.youtube.com/watch?v=K3t6s-dMQuY",
+    date: "12/09/2020",
+  },
+  {
+    id: "7",
+    title: "Cubos Bits",
+    description: "O que significa Bit?",
+    imageURI:
+      "https://user-images.githubusercontent.com/21963291/194651640-41fe3ce8-c6a9-4d7e-bb9a-49bdb3087f6a.png",
+    url: "https://www.youtube.com/watch?v=2HRqr8Zrl14",
+    date: "23/01/2020",
+  },
+];
