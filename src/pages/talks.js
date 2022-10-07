9128e90157868d7d98258e4d1d71057756ca2f4d
ildaneta <iildaemanoely@gmail.com>
2022-10-07 23:26:02 +0200

    feat: create talks pages and its styles

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -1,22 +1,47 @@
 import React from "react";
 import Layout from "../components/Layout";
 import SquareVideo from "../components/SquareVideo";
+import { talks, participations } from "../utils/talks";
 
 import Styled from "../styles/talks";
 
 const Talks = () => {
   return (
     <Layout>
-      <text>Participações no Youtube</text>
       <Styled.Container>
-        <Styled.teste>
-          <SquareVideo />
-          <SquareVideo />
-          <SquareVideo />
-          <SquareVideo />
-          <SquareVideo />
-          <SquareVideo />
-        </Styled.teste>
+        <Styled.ContainerTitle>
+          <Styled.Title>Talks</Styled.Title>
+        </Styled.ContainerTitle>
+
+        <Styled.ContainerTalks>
+          {talks.map((talk) => (
+            <SquareVideo
+              key={talk.id}
+              description={talk.description}
+              title={talk.title}
+              url={talk.url}
+              imageURI={talk.imageURI}
+            />
+          ))}
+        </Styled.ContainerTalks>
+
+        <Styled.Divider />
+
+        <Styled.ContainerTitle>
+          <Styled.Title>Participações no Youtube</Styled.Title>
+        </Styled.ContainerTitle>
+
+        <Styled.ContainerTalks>
+          {participations.map((participation) => (
+            <SquareVideo
+              key={participation.id}
+              description={participation.description}
+              title={participation.title}
+              url={participation.url}
+              imageURI={participation.imageURI}
+            />
+          ))}
+        </Styled.ContainerTalks>
       </Styled.Container>
     </Layout>
   );
--- src/styles/talks.js
+++ src/styles/talks.js
@@ -1,24 +1,70 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
+  flex-direction: column;
+  margin-top: 20px;
 `;
 
-const teste = styled.div`
+const ContainerTalks = styled.div`
   display: flex;
   flex-wrap: wrap;
-  max-width: 1300px;
+  max-width: 1080px;
   align-items: center;
   justify-content: center;
 `;
 
-const Image = styled.img``;
+const Title = styled.h1`
+  display: flex;
+  width: 70%;
+  line-height: 3.2rem;
+  font-family: "Didact Gothic", sans-serif;
+  font-size: 2.8rem;
+  font-weight: 600;
+  color: var(--colorLetters);
+
+  ${media.lessThan("huge")`
+    margin-left: 100px;
+    font-size: 1.8rem;
+    line-height: 2rem;
+    width: 400px;
+  `};
+
+  @media screen and (max-width: 1300px) {
+    margin-left: 50px;
+  }
+`;
+
+const ContainerTitle = styled.div`
+  display: flex;
+  width: 1080px;
+  margin-left: 20px;
+
+  ${media.lessThan("huge")`
+    max-width: 850px
+  `}
+
+  ${media.lessThan("medium")`
+    width: 480px;
+  `};
+`;
+
+const Divider = styled.div`
+  margin-top: 100px;
+
+  ${media.lessThan("huge")`
+    margin-top: 60px;
+  `}
+`;
 
 export default {
   Container,
-  teste,
-  Image,
+  ContainerTalks,
+  Title,
+  ContainerTitle,
+  Divider,
 };
