19c252bd1edbb931747c462eb8cab7ec73c547ad
ildaneta <iildaemanoely@gmail.com>
2022-10-12 23:14:18 +0200

    feat: create colors file

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  colorNeutral0: "#000",
+  colorNeutral1: "#121212",
+  colorNeutral2: "#222222",
+  colorNeutral3: "#333333",
+  colorNeutral4: "#555555",
+  colorNeutral5: "#888888",
+  colorNeutral6: "#E5E5E5",
+  colorNeutral7: "#F1F1F1",
+  colorNeutral8: "#FDFDFD",
+  colorNeutral9: "#DFE2E5",
+  colorNeutral10: "#5F5F5F",
+};
