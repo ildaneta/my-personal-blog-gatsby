85fb7b6a110e928c6a907295cc1a79e9b156004e
ildaneta <iildaemanoely@gmail.com>
2022-10-12 22:57:16 +0200

    feat: add theme provider into gatsby browser configs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,9 @@
-import 'lazysizes';
-require('prismjs/themes/prism-tomorrow.css');
+import "lazysizes";
+import React from "react";
+import { ThemeProvider } from "./src/context/Theme";
+
+require("prismjs/themes/prism-tomorrow.css");
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider>{element}</ThemeProvider>
+);
