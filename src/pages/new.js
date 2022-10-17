4a05de3c1b1803429104a2df8270a01ee16abbfc
ildaneta <iildaemanoely@gmail.com>
2022-10-17 22:35:00 +0200

    feat: create new page structure

---
+++ src/pages/new.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout2 from "../components/Layout2";
+import Styled from "../styles/new";
+
+const New = () => {
+  return (
+    <Layout2>
+      <text></text>
+    </Layout2>
+  );
+};
+
+export default New;
