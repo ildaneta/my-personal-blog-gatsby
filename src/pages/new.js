e55863a0b1aaee6644fa76018a60950331196304
ildaneta <iildaemanoely@gmail.com>
2023-02-11 22:07:25 +0100

    refactor: improve text about me

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Button from "../components/Button";
 import Layout2 from "../components/Layout2";
+import Seo from "../components/seo";
 
 import IllustraSVG from "../images/illustra-home.svg";
 
@@ -8,31 +10,41 @@ import Styled from "../styles/new";
 const New = () => {
   return (
     <Layout2>
+      <Seo title="Home" />
+
       <Styled.ContainerAboutMe>
         <Styled.ContainerIllustration>
           <Styled.Illustration src={IllustraSVG} alt="Illustration home" />
         </Styled.ContainerIllustration>
 
-        <Styled.ContainerAboutMeText>
-          <Styled.WelcomeTitle>
-            Seja bem vindo(a) ao meu blog!
-          </Styled.WelcomeTitle>
-          <Styled.WelcomeDescription>
-            Meu nome é Ilda Neta. Sou desenvolvedora mobile 📱 e amo construir
-            aplicações lindas e usuais, sempre tendo como foco a experiência do
-            usuário.
-          </Styled.WelcomeDescription>
-          <Styled.WelcomeDescription>
-            Além de produzir conteúdos em meu blog, estou ativamente
-            compartilhando conhecimento no LinkedIn e em eventos de tecnologia
-            como palestrante. 👩🏻‍🎤
-          </Styled.WelcomeDescription>
-          <Styled.WelcomeDescription>
-            Para saber um pouco mais sobre mim e minha trajetória, clique
-            abaixo. ⬇️
-          </Styled.WelcomeDescription>
-        </Styled.ContainerAboutMeText>
+        <div>
+          <Styled.ContainerAboutMeText>
+            <Styled.WelcomeTitle>
+              Seja bem vindo(a) ao meu blog!
+            </Styled.WelcomeTitle>
+            <Styled.WelcomeDescription>
+              Meu nome é Ilda Neta. Sou desenvolvedora mobile 📱 e amo construir
+              aplicações lindas e usuais, sempre tendo como foco a experiência
+              do usuário.
+            </Styled.WelcomeDescription>
+            <Styled.WelcomeDescription>
+              Além de produzir conteúdos em meu blog, estou ativamente
+              compartilhando conhecimento no LinkedIn e em eventos de tecnologia
+              como palestrante. 👩🏻‍🎤
+            </Styled.WelcomeDescription>
+            <Styled.WelcomeDescription>
+              Para saber um pouco mais sobre mim e minha trajetória, clique
+              abaixo. ⬇️
+            </Styled.WelcomeDescription>
+          </Styled.ContainerAboutMeText>
+
+          <Button label="Sobre mim" url="/aboutme" />
+        </div>
       </Styled.ContainerAboutMe>
+
+      <section>
+        <h2>Artigos mais recentes</h2>
+      </section>
     </Layout2>
   );
 };
