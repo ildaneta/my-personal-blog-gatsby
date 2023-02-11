d682f2ed0c5f012d2f22e9815fbd32fe1684ae0b
ildaneta <iildaemanoely@gmail.com>
2023-02-11 22:05:32 +0100

    feat: create articles page

---
+++ src/pages/articles.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout2 from "../components/Layout2";
+import Seo from "../components/seo";
+
+const Articles = () => {
+  return (
+    <Layout2>
+      <Seo title="Artigos" />
+      <text>Artigos</text>
+    </Layout2>
+  );
+};
+
+export default Articles;
